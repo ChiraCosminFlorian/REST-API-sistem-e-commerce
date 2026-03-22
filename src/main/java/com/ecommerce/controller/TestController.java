629a8b215fee42242e406514c591a1488bd78bf8
ChiraCosminFlorian <chiracosminflorian@gmail.com>
2026-03-22 20:26:41 +0200

    added missing import for PreAuthorize in TestController and now i can create categories

--- src/main/java/com/ecommerce/controller/TestController.java
+++ src/main/java/com/ecommerce/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ public class TestController {
     }
 
     @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> adminEndpoint() {
         return ResponseEntity.ok("admin works");
     }
